06e86866dc2d0384854d6dc4b965ba747c25bee6
Baz <jennigoldstein@gmail.com>
2018-08-10 13:11:36 -0500

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -111,7 +111,7 @@ $(document).ready(function () {
         newCardDiv = $("<div class='card card-body mt-3 mb-3'>");
         newMediaDiv = $("<div class='media'>");
         newCardDiv.append(newMediaDiv);
-        newImageTag = $("<img class='align-self-start mr-3' " + "src=" + parkImage + "alt='park-image'>")
+        newImageTag = $("<img class='align-self-start mr-3' " + "src=" + parkImage + " alt='park-image'>")
         newMediaDiv.append(newImageTag);
         newMediaBodyDiv = $("<div class='media-body'>")
         newMediaDiv.append(newMediaBodyDiv);
@@ -119,23 +119,23 @@ $(document).ready(function () {
             "<h5 class='mt-0'>" + parkName +
             "<h6 class='card-subtitle mb-2 text-muted'>" + milesAway +
             "<p>" + "Recent Check Ins:" + checkIns + "<br>" +
-            "<a href='park.html' class='btn btn-primary'>" + "More Info" + "</a>"
+            "<a href='park.html' class='btn btn-primary more-info'>" + "More Info" + "</a>"
 
         )
         $("#listWrapper").append(newCardDiv);
 
-        //when a pin is clicked populate the park.html page
-        //$("#").on("click", function () {
-            // $("#park-name").text(parkName);
-            // $("#miles-away").text(milesAway);
-            // $("#recent-check-ins").text(checkIns);
-            // $("#leash").text(leashCheck);
-            // $("#fence").text(fenceCheck);
-            // $("#swim").text(swimCheck);
-            // $("#shade").text(shadeCheck);
-            // $("#picnic").text(picnicCheck);
-            // $("#water").text(waterCheck);
-        //});
+        //when more info is clicked on list page populate the park.html page
+        $(document).on("click", ".more-info", function () {
+            $("#park-name").text(parkName);
+            $("#miles-away").text(milesAway);
+            $("#recent-check-ins").text(checkIns);
+            $("#leash").text(leashCheck);
+            $("#fence").text(fenceCheck);
+            $("#swim").text(swimCheck);
+            $("#shade").text(shadeCheck);
+            $("#picnic").text(picnicCheck);
+            $("#water").text(waterCheck);
+        });
 
 
 
