4b8315fecce4a727b5a00123e991beb9dc1a5e67
Baz <jennigoldstein@gmail.com>
2018-08-09 19:47:11 -0500

    updates to firebase.js

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -13,56 +13,89 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-    //when submit button is clicked, push to database
-    $("#submit-btn").on("click", function () {
+    //when submit button is clicked, push form info to database
+    $("#submit-btn").on("click", function (event) {
+        event.preventDefault()
         var parkName = $("#park-name-input").val().trim();
         var location = $("#location-input").val().trim();
-        var leashCheck = $("#leash-check").val().trim();
-        var fenceCheck = $("#fence-check").val().trim();
-        var swimCheck = $("#swim-check").val().trim();
-        var shadeCheck = $("#shade-check").val().trim();
-        var picnicCheck = $("#picnic-check").val().trim();
-        var waterCheck = $("#water-check").val().trim();
+        var leashCheck = false;
+        if(($("#leash-check").is(":checked"))){
+            leashCheck = ("Off-Leash");
+        }
+        else{
+            leashCheck =("Requires Leash");
+        }
+        var fenceCheck = false;
+        if(($("#fence-check").is(":checked"))){
+            fenceCheck = ("Fenced In");
+        }
+        else{
+            fenceCheck =("Not Fenced-In");
+        }
+        var swimCheck = false;
+        if(($("#swim-check").is(":checked"))){
+            swimCheck = ("Swimming Hole");
+        }
+        else{
+            swimCheck =("No Swimming Hole");
+        }
+        var shadeCheck = false;
+        if(($("#shade-check").is(":checked"))){
+            shadeCheck = ("Shaded Areas");
+        }
+        else{
+            shadeCheck =("No Shaded Areas");
+        }
+        var picnicCheck = false;
+        if(($("#picnic-check").is(":checked"))){
+            picnicCheck = ("Picnic Tables");
+        }
+        else{
+            picnicCheck =("No Picnic Tables");
+        }
+        var waterCheck = false;
+        if(($("#water-check").is(":checked"))){
+            waterCheck = ("Water Fountains");
+        }
+        else{
+            waterCheck =("No Water Fountains");
+        }
 
         database.ref().push({
             parkName: parkName,
             location: location,
-            options: {
-                leashCheck: leashCheck,
-                fenceCheck: fenceCheck,
-                swimCheck: swimCheck,
-                shadeCheck: shadeCheck,
-                picnicCheck: picnicCheck,
-                waterCheck: waterCheck
-            }
-
-        })
-
-    });
-
-    //when a new park is added send to html
-    database.ref().on("child_added", function (childSnapshot) {
-        var parkName = childSnapshot.val().parkName;
-        //var milesAway=
-        // var recentCheckIns=
-        var recentCheckins = childSnapshot.val().parkName;
-
-
-        //push to park page
-        for (var i = 0; i < childSnapshot.options.length; i++) { 
-            if(childSnapshot.options[i]===true){
-                var item= childSnapshot.val().options[i];
-                var newListItem = $("<li>");
-                newListItem.text(item);
-
-
-            }
-        }
-               
+            leashCheck: leashCheck,
+            fenceCheck: fenceCheck,
+            swimCheck: swimCheck,
+            shadeCheck: shadeCheck,
+            picnicCheck: picnicCheck,
+            waterCheck: waterCheck
+        
 
         });
+//clear input boxes and reset the checkboxes
+        $("#park-name-input").val("");
+        $("#location-input").val("");;
+        $("input[type=checkbox]").prop('checked',false);
+        
+
+});
+
+//when a new park is added send to html
+database.ref().on("child_added", function (childSnapshot) {
+    var parkName = childSnapshot.val().parkName;
+    //var milesAway=
+    // var recentCheckIns=
+    
+
+
+   
+
+
+});
 
 
     });
+
 
 //onclick for checkin-button
